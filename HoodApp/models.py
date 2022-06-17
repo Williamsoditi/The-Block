af409ecb23bb03dc8686772397704a3e52b107c0
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-17 12:46:12 +0300

    Feat : Adds profile model

--- HoodApp/models.py
+++ HoodApp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
 
 # Neighbourhood model
 class Neighbourhood(models.Model):
@@ -48,4 +49,26 @@ class Location(models.Model):
     def delete_location(self):
         self.delete()
 
+#PROFILE 
+class Profile(models.Model):
+    prof_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=1000, null=True)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True,related_name='profile')
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
