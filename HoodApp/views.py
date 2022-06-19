35d1f76d66f1dd3afaafa3ca88c93b061dd04e3c
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-19 03:04:16 +0300

    Feat : Adds profile views - This adds create_profile, update_profile nad profile views

--- HoodApp/views.py
+++ HoodApp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect, Http404
 from . models import *
+from .forms import *
 from django.contrib.auth.models import User
 
 
@@ -12,6 +13,46 @@ from django.contrib.auth.models import User
 @csrf_exempt
 def home(request):    
     current_user = request.user
-    # profile.user = current_user
+    profile.user = current_user
 
     return render(request,'home.html')
+
+@csrf_exempt
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    title = "Create Profile"
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+
+    else:
+        form = ProfileForm()
+    return render(request, 'profile/create_profile.html', {"form": form, "title": title})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    neighbourhood = Neighbourhood.objects.all()
+
+
+    return render(request,"profile/profile.html",{'profile':profile,'neighbourhood':neighbourhood})
+
+def update_profile(request,id):
+    user = User.objects.get(id=id)
+    profile = Profile.objects.get(user = user)
+    form = UpdateProfileForm(instance=profile)
+    if request.method == "POST":
+            form = UpdateProfileForm(request.POST,request.FILES,instance=profile)
+            if form.is_valid():  
+                
+                profile = form.save(commit=False)
+                profile.save()
+                return redirect('/profile') 
+            
+    return render(request, 'profile/update_profile.html', {"form":form})
